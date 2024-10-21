9dbb645c9a6fa140cb30954c7db8b569a49fa4dd
zxhhhxz1221 <1305894626@qq.com>
2024-10-21 17:11:35 +0800

    真正从数据库获取博客数据并展示在页面 ...
    - pom.xml 增加 mysql-connector-j 依赖
    - JDBC 的代码写法，记住一次就好了，其他都类似。属于模板方法，或者说“重复”的内容，后续我们会给自动化

--- src/main/java/io/github/zxh111222/blog/MyDBUtil.java
+++ src/main/java/io/github/zxh111222/blog/MyDBUtil.java
@@ -13,7 +13,7 @@ public class MyDBUtil {
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/home", "root", "secret_mysql");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/home", "root", "123456");
             System.out.println("Connected to database");
         } catch (Exception e) {
             throw new RuntimeException(e);
