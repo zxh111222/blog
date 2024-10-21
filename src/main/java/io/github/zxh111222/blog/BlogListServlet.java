e71648bd93161dbcff21327f02d80a3068879c66
zxhhhxz1221 <1305894626@qq.com>
2024-10-21 16:56:44 +0800

    /blog-list => 由BlogListServlet处理，内部跳转到-> /WEB-INF/blog-list.jsp

---
+++ src/main/java/io/github/zxh111222/blog/BlogListServlet.java
@@ -0,0 +1,17 @@
+package io.github.zxh111222.blog;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/blog-list")
+public class BlogListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/blog-list.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/blog-list.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lenovo
+  Date: 2024/10/21
+  Time: 16:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>博客列表 - Blog</title>
+</head>
+<body>
+
+</body>
+</html>
