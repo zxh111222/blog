9a9919dca81a9958b2f013ab576222fe780c2360
zxhhhxz1221 <1305894626@qq.com>
2024-10-22 19:56:14 +0800

    add-blog.html 提交信息给服务端,并在服务端增加 Servlet 界面客户端提交过来的数据

---
+++ src/main/java/io/github/zxh111222/blog/BlogAddServlet.java
@@ -0,0 +1,26 @@
+package io.github.zxh111222.blog;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/add-blog1")
+public class BlogAddServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+
+        String title = req.getParameter("title");
+        System.out.println("博客标题：" + title);
+        String content = req.getParameter("content");
+        System.out.println("博客内容：" + content);
+
+        writer.write("博客保存成功！");
+    }
+}
