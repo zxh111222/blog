d72b562e66c80e9faeb8ba9948d820ed70a96a1f
zxhhhxz1221 <1305894626@qq.com>
2024-10-23 19:04:46 +0800

    BlogAddServlet.java 更名

--- src/main/java/io/github/zxh111222/blog/BlogAddServlet.java
+++ src/main/java/io/github/zxh111222/blog/BlogAddServlet.java
@@ -7,24 +7,56 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-@WebServlet("/add-blog1")
+@WebServlet("/add-blog")
 public class BlogAddServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/add-blog.jsp").forward(req, resp);
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        PrintWriter writer = resp.getWriter();
-
-        String title = req.getParameter("title");
-        System.out.println("博客标题：" + title);
-        String content = req.getParameter("content");
-        System.out.println("博客内容：" + content);
-        String type = req.getParameter("type");
-        System.out.println("博客类型：" + type);
 
         // todo: 1. 接收用户数据、2.验证数据、3.处理数据（保存数据库）4.响应内容 ...
 
-        writer.write("博客保存成功！");
+        String title = req.getParameter("title");
+        String content = req.getParameter("content");
+        String type = req.getParameter("type");
+
+        Connection connection = MyDBUtil.getConnection();
+        String sql = "INSERT INTO blog (title, content, type) VALUES (?, ?, ?)";
+        PreparedStatement pstmt = null;
+        try {
+            pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, title);
+            pstmt.setString(2, content);
+            pstmt.setString(3, type);
+
+            int affectedRows = pstmt.executeUpdate();
+
+            if (affectedRows > 0) {
+                // 跳转
+                resp.sendRedirect(req.getContextPath() + "/blog-list");
+            } else {
+                resp.getWriter().println("添加博客失败");
+            }
+        } catch (SQLException e) {
+            resp.getWriter().println("添加博客时发生错误: " + e.getMessage());
+        } finally {
+            // 关闭资源
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 }
