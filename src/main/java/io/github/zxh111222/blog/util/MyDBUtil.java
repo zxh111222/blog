d37f03d1a00ff95d86ab2445b3c72da0843ebae0
zxhhhxz1221 <1305894626@qq.com>
2024-10-28 16:07:37 +0800

    druid: 数据库连接池技术之一 ...
    https://github.com/alibaba/druid

--- src/main/java/io/github/zxh111222/blog/util/MyDBUtil.java
+++ src/main/java/io/github/zxh111222/blog/util/MyDBUtil.java
@@ -1,21 +1,36 @@
 package io.github.zxh111222.blog.util;
 
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class MyDBUtil {
-    private static Connection connection;
+    private static final DataSource dataSource;
 
-    public synchronized static Connection getConnection() {
-        if (connection != null) {
-            return connection;
-        }
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/home", "root", "Lmysqldlmm@126");
-            System.out.println("Connected to database");
+    static {
+        //1.引入依赖
+        //2.提供配置文件
+        //3.load 配置文件
+        Properties prop = new Properties();
+        try (InputStream inputStream = MyDBUtil.class.getClassLoader().getResourceAsStream("druid.properties")) {
+            prop.load(inputStream);
+            //4. 获取数据库连接池对象 DataSource
+            dataSource = DruidDataSourceFactory.createDataSource(prop);
         } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConnection() {
+        Connection connection;
+        try {
+            //5. 获取数据库连接 Connection
+            connection = dataSource.getConnection();
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return connection;
